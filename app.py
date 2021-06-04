96ba7e3f46a730807fd7c9b7bce0357f37bbf370
Tuomas Poukkula <66461922+tuopouk@users.noreply.github.com>
2021-06-05 00:11:41 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -331,4 +331,4 @@ app.config['suppress_callback_exceptions']=True
 app.layout = serve_layout
 # Ajetaan sovellus.
 if __name__ == '__main__':
-    app.run_server(debug=True)   
+    app.run_server(debug=False)   
